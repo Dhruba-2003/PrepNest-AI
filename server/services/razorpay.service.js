462385d0fd3f70047755dd83c66b78b5049ef2c9
banerjeesabuj1234 <sabujbanerjee420@gmail.com>
2026-07-14 12:22:59 +0530

    some file updates

--- server/services/razorpay.service.js
+++ server/services/razorpay.service.js
@@ -2,9 +2,10 @@ import dotenv from "dotenv"
 dotenv.config()
 import Razorpay from "razorpay"
 
+
 const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
+  key_id: RAZORPAY_KEY_ID,
+  key_secret: RAZORPAY_KEY_SECRET,
 });
 
 export default razorpay
